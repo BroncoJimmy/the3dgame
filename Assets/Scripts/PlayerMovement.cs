8b89c5dc8a5045f8c63aa884cc4ff3de68623eef
RobertEscorcio <robertescorcio@gmail.com>
2022-01-03 20:25:51 -0800

    added unity project files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private float lastX = 0;
+    private float lastZ = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if  (x != lastX || z != lastZ) {
+            print($"x={x}, z={z}, velocity.x={velocity.x},velocity.y={velocity.y}");
+            lastX = x;
+            lastZ = z;
+        }
+        
+       // if (Input.GetButtonDown("Vertical"))
+       // {
+       //     velocity.x = -3f;
+       // }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
